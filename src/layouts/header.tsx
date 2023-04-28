290e596e17d3e30ec4ad09b4621615c01ce0e3ba
kuromika <kuromika.dev@gmail.com>
2023-04-28 15:01:15 -0500

    Add text banner to header

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Navigation } from "./nav-bar";
 import styles from "@/styles/layouts/Header.module.css";
 
@@ -5,6 +6,10 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <Navigation></Navigation>
+      <Link href="/" className={styles.banner}>
+        <span className={styles.romaji}>Natsu no Daisankaku</span>
+        <span className={styles.kanji}>夏の大三角</span>
+      </Link>
     </header>
   );
 };
