cfa6f084bc9349a5a562af4cb530644d0746a140
kuromika <kuromika.dev@gmail.com>
2023-04-29 14:23:01 -0500

    Add auth provider to layout

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren } from "react";
 import { Header } from "./header";
+import { AuthProvider } from "@/contexts/auth-context";
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <Header></Header>
-      <main>{children}</main>
+      <AuthProvider>
+        <Header></Header>
+        <main>{children}</main>
+      </AuthProvider>
     </>
   );
 };
