24ae363196ef15d0fd80b1395a7fb41480853db1
kuromika <kuromika.dev@gmail.com>
2023-04-30 09:37:09 -0500

    Increase main element height

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,13 +1,14 @@
 import { PropsWithChildren } from "react";
 import { Header } from "./header";
 import { AuthProvider } from "@/contexts/auth-context";
+import styles from '@/styles/layouts/Layout.module.css'
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <AuthProvider>
         <Header></Header>
-        <main>{children}</main>
+        <main className={styles.main}>{children}</main>
       </AuthProvider>
     </>
   );
