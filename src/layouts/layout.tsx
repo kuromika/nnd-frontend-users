7c421c53932fc7763113eec3daa4299a42e9ab84
kuromika <kuromika.dev@gmail.com>
2023-05-01 14:29:57 -0500

    Move auth context to app & implement auth guard

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,15 +1,13 @@
 import { PropsWithChildren } from "react";
 import { Header } from "./header";
 import { AuthProvider } from "@/contexts/auth-context";
-import styles from '@/styles/layouts/Layout.module.css'
+import styles from "@/styles/layouts/Layout.module.css";
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <AuthProvider>
-        <Header></Header>
-        <main className={styles.main}>{children}</main>
-      </AuthProvider>
+      <Header></Header>
+      <main className={styles.main}>{children}</main>
     </>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
+import { AuthGuard } from "@/components/auth-guard";
+import { AuthProvider } from "@/contexts/auth-context";
 import { Layout } from "@/layouts/layout";
 import "@/styles/globals.css";
+import { NextComponentType } from "next";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+export type CustomAppProps = AppProps & {
+  Component: NextComponentType & { blockAuth?: boolean };
+};
+
+export default function App({ Component, pageProps }: CustomAppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthProvider>
+      {Component.blockAuth ? (
+        <AuthGuard>
+          <Layout>
+            <Component {...pageProps}></Component>
+          </Layout>
+        </AuthGuard>
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </AuthProvider>
   );
 }
